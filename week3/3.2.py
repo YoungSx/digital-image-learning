bf7aaa662ea42c8f4d54179e9cb41f7bab714659
Shangxin <shangxin@outlook.com>
2019-10-03 00:09:18 +0800

    fix: tidu

--- week3/3.2.py
+++ week3/3.2.py
@@ -3,20 +3,18 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
-def template2(x1, y1, arr):
-    sum = 0
-    res = abs(arr[x1 + 1, y1] - arr[x1, y1]) + abs(arr[x1, y1 + 1] - arr[x1, y1])
+def gradientTemplate(x1, y1, arr):
+    res = abs(int(arr[x1 + 1, y1]) - int(arr[x1, y1])) + abs(int(arr[x1, y1 + 1]) - int(arr[x1, y1]))
     return res
 
-img1 = Image.open('./static/leina.jpg').convert('L')
+img1 = Image.open('./static/test3.jpg').convert('L')
 img1.show()
 a1 = np.array(img1)
 a2 = np.array(a1)
 x, y = a2.shape
 for i in range(x - 1):
     for j in range(y - 1):
-        a2[i, j] = template2(i, j, a1)
-
+        a2[i, j] = gradientTemplate(i, j, a1)
 img2 = Image.fromarray(a2)
 img2.show()
 
